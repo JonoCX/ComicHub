43e4a6c0d4a79e1ac7b5f86f68341797fd18e2d3
JonoCX <j.carlton@newcastle.ac.uk>
2016-04-03 18:42:31 +0100

    commenting done

--- app/src/main/java/uk/ac/ncl/j_carlton/comichub/ViewItem.java
+++ app/src/main/java/uk/ac/ncl/j_carlton/comichub/ViewItem.java
@@ -25,10 +25,18 @@ public class ViewItem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_item);
+
+        // assign the passed comic book object to the one within the scope of the class
         comicBook = getIntent().getParcelableExtra("ComicBook");
+
+        // get the intent of the parent activity
         String activityIntent = getIntent().getStringExtra("intent");
+
+        // if the user has performed a search and came to the this comic book then
+        // get the search query
         if (getIntent().hasExtra("search"))
             searchQuery = getIntent().getStringExtra("search");
+
         // Set image
         ImageView coverArt = (ImageView) findViewById(R.id.coverArt);
         String imageRef = comicBook.getImageRef();
@@ -57,6 +65,7 @@ public class ViewItem extends AppCompatActivity {
             published.setText("");
         }
 
+        // if the intent of the calling activity is to add to the library
         if (activityIntent.equals("add")) {
             addToLibrary();
         } else {
@@ -65,6 +74,10 @@ public class ViewItem extends AppCompatActivity {
 
     }
 
+    /**
+     *  Transforms the button on the view to an "add to library"
+     *  button.
+     */
     private void addToLibrary() {
         Button button = (Button) findViewById(R.id.add_remove_btn);
         button.setText("Add to Library");
@@ -77,56 +90,105 @@ public class ViewItem extends AppCompatActivity {
         });
     }
 
+    /**
+     *  Transforms the button on the view to an "remove from library"
+     *  button
+     */
     private void removeFromLibrary() {
         Button button = (Button) findViewById(R.id.add_remove_btn);
         button.setText("Remove from Library");
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // false to remove from the library
                 btnOnClick(false);
             }
         });
     }
 
+    /**
+     * Performs the action of either removing or adding to the library
+     * @param result    true = add & false = remove
+     */
     private void btnOnClick(boolean result) {
         Boolean[] res = new Boolean[1];
         res[0] = result;
         if (result) {
             try {
-                boolean success = new updateComicBook().execute(res).get();
-                Toast.makeText(getBaseContext(), "Added to Library", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(this, AddToLibrary.class);
-                intent.putExtra("search", searchQuery);
-                startActivity(intent);
+                // perform the update
+                boolean success = new UpdateComicBook().execute(res).get();
+
+                // signal to the user that it was successful
+                if (success) {
+                    Toast.makeText(getBaseContext(), "Added to Library", Toast.LENGTH_LONG).show();
+
+                    // reload the add to library activity and pass the query to perform the search again
+                    Intent intent = new Intent(this, AddToLibrary.class);
+                    intent.putExtra("search", searchQuery);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getBaseContext(), "Unsuccessful", Toast.LENGTH_LONG).show();
+                }
             } catch (InterruptedException | ExecutionException e) {
                 e.printStackTrace();
             }
         }
         else {
             try {
-                boolean success = new updateComicBook().execute(res).get();
-                Toast.makeText(getBaseContext(), "Removed from Library", Toast.LENGTH_LONG).show();
+                boolean success = new UpdateComicBook().execute(res).get();
+
+                // signal to the user that it was successful
+                if (success) {
+                    Toast.makeText(getBaseContext(), "Removed from Library", Toast.LENGTH_LONG).show();
+
+                    // reload the add to library activity and pass the query to perform the search again
+                    Intent intent = new Intent(this, ViewLibrary.class);
+                    intent.putExtra("search", searchQuery);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getBaseContext(), "Unsuccessful", Toast.LENGTH_LONG).show();
+                }
+
             } catch (InterruptedException | ExecutionException e) {
                 e.printStackTrace();
             }
         }
     }
 
+    /**
+     * AsyncTask to spawn a new thread to connect to the comic book database
+     * on AWS server and update a comic book to be either in or out of the
+     * library
+     */
+    private class UpdateComicBook extends AsyncTask<Boolean, Void, Boolean> {
 
-    private class updateComicBook extends AsyncTask<Boolean, Void, Boolean> {
+        // server and database connection parameters
         private final String dbURL = "jdbc:mysql://mysqldbinstance.cchftrgjl5qm.eu-west-1.rds.amazonaws.com:3306/comichub";
         private final String dbUserName = "comicaccess";
         private final String dbPass = "Password123";
 
+        /**
+         * method to perform the task in the background
+         * @param params    optional string parameters
+         * @return          the response from carrying out the task
+         */
         @Override
         protected Boolean doInBackground(Boolean... params) {
             boolean success = false;
+
+            // fetch the passed result, true if they want to add and false if they don't
             boolean param = params[0];
             try {
+                // fetch the jdbc driver class
                 Class.forName("com.mysql.jdbc.Driver");
+
+                // make the connection to the server and database
                 Connection connection = DriverManager.getConnection(dbURL, dbUserName, dbPass);
                 String query;
+
+                // if they want to add the comic book to the library
                 if (param) {
+                    // update the row and change the object
                     query = "UPDATE `comics` SET `inLibrary`=1 WHERE `id`=" + comicBook.getId();
                     comicBook.setInLibrary(true);
                 }
@@ -134,9 +196,15 @@ public class ViewItem extends AppCompatActivity {
                     query = "UPDATE `comics` SET `inLibrary`=0 WHERE `id`=" + comicBook.getId();
                     comicBook.setInLibrary(false);
                 }
+
+                // create and execute the statement
                 Statement statement = connection.createStatement();
                 statement.executeUpdate(query);
+
+                // close connection
                 connection.close();
+
+                // successful.
                 success = true;
             } catch (ClassNotFoundException | SQLException e) {
                 e.printStackTrace();
--- app/src/main/java/uk/ac/ncl/j_carlton/comichub/landingscreen.java
+++ app/src/main/java/uk/ac/ncl/j_carlton/comichub/landingscreen.java
@@ -2,10 +2,15 @@ package uk.ac.ncl.j_carlton.comichub;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+/**
+ * The class associated with the landing screen activity.
+ *
+ * @author Jonathan Carlton - 130266400
+ */
 public class LandingScreen extends AppCompatActivity {
 
     @Override
@@ -14,24 +19,52 @@ public class LandingScreen extends AppCompatActivity {
         setContentView(R.layout.activity_landingscreen);
     }
 
+    /**
+     * If the 'find stores near me' button is clicked, this method
+     * is called. It loads the google maps app installed on the
+     * android operating system and performs a search.
+     * @param v     the view
+     */
     public void openGoogleMapsSearch(View v) {
+        // the search query - comic books stores in the area of the phone
         Uri gmmIntentUri = Uri.parse("geo:0,0?q=comic book store");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+
+        // set package to that of the google maps app
         mapIntent.setPackage("com.google.android.apps.maps");
+
+        // then start it
         startActivity(mapIntent);
     }
 
+    /**
+     * If the 'logout' button is pressed, this method is called.
+     * @param v     the view
+     */
     public void logout(View v) {
+        // re-direct back to the starting activity
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * If the 'add to library' button is click, this method
+     * is called.
+     * @param v     the view
+     */
     public void addToLibrary(View v) {
+        // direct to the add to library activity
         Intent intent = new Intent(this, AddToLibrary.class);
         startActivity(intent);
     }
 
+    /**
+     * If the 'view library' button is click, this method
+     * is called.
+     * @param v     the view
+     */
     public void viewLibrary(View v) {
+        // direct to the view library activity
         Intent intent = new Intent(this, ViewLibrary.class);
         startActivity(intent);
     }
